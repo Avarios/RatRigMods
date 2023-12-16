778683996a882c4d0279c43cf8df6197f7080701
Pawel Warmuth <warmuth.pawel@gmail.com>
2023-12-16 10:12:54 +0100

    added sveltekit project

---
+++ src/lib/icons/index.ts
@@ -0,0 +1,5 @@
+export {default as AppleTouchIcon} from './apple-touch-icon.png';
+export {default as Favicon32} from './favicon-32x32.png';
+export {default as Favicon16} from './favicon-16x16.png';
+export {default as Manifest} from './site.webmanifest';
+export {default as SafariPinnedTab} from './safari-pinned-tab.svg';
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { NavBar } from '$lib';
+	import { AppleTouchIcon, Favicon16, Favicon32, Manifest, SafariPinnedTab } from '$lib/icons';
+</script>
+
+<svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href="{AppleTouchIcon}" />
+	<link rel="icon" type="image/png" sizes="32x32" href={Favicon32} />
+	<link rel="icon" type="image/png" sizes="16x16" href={Favicon16} />
+	<link rel="manifest" href={Manifest} />
+	<link rel="mask-icon" href={SafariPinnedTab} color="#5bbad5" />
+	<title>RatRig Mod Database</title>
+</svelte:head>
+
+<div class="container mx-auto px-4">
+	<NavBar></NavBar>
+	<slot />
+</div>
